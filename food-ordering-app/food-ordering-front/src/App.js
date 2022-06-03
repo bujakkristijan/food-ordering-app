7464320dbc8034c1a046f57169bad90a746ead1d
Kristijan Bujak <cileb411@gmail.com>
2022-06-03 23:38:13 +0200

    dodat router u app.js

--- food-ordering-app/food-ordering-front/src/App.js
+++ food-ordering-app/food-ordering-front/src/App.js
@@ -3,13 +3,25 @@ import './App.css';
 import ListUserComponent from './components/ListUserComponent';
 import NavbarComponent from './components/NavbarComponent';
 import FooterComponent from './components/FooterComponent';
+/*import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; */
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
+//od V6, nema SWITCH, vec je zamenjeno sa ROUTES, component sa element i nije vise {ListUserComponent} vec {<ListUserComponent/>}
 function App() {
   return (
     <div>
-      <NavbarComponent/>
-      <ListUserComponent/>
-      <FooterComponent/>
+      <Router>
+        <NavbarComponent/>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element = {<ListUserComponent/>}></Route> 
+            <Route path='/users' element = {<ListUserComponent/>}></Route>
+          </Routes>
+        </div>
+        
+        <FooterComponent/>
+      </Router>
+      
     </div>
   );
 }
