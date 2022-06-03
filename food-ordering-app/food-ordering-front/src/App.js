9e97713dc2558f7366ce012b650e4ad91c9e4a29
Kristijan Bujak <cileb411@gmail.com>
2022-06-03 22:02:15 +0200

    zapocet header i footer

--- food-ordering-app/food-ordering-front/src/App.js
+++ food-ordering-app/food-ordering-front/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import ListUserComponent from './components/ListUserComponent';
+import NavbarComponent from './components/NavbarComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
     <div>
+      <NavbarComponent/>
       <ListUserComponent/>
+      <FooterComponent/>
     </div>
   );
 }
---
+++ food-ordering-app/food-ordering-front/src/components/FooterComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>
+                All rights reserved Kristijan Bujak 2022
+            </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
