eb9048cd95fc7368feabdc81770bc15a8ce8109b
Kristijan Bujak <kbujak996@gmail.com>
2023-11-17 19:38:45 +0100

    izmena izgleda korpe, nije zavrseno jos

--- food-ordering-app/food-ordering-front/src/components/cart/CartComponent.js
+++ food-ordering-app/food-ordering-front/src/components/cart/CartComponent.js
@@ -218,8 +218,13 @@ const CartComponent = () => {
 
   return (
     <>
-    <div className='container'>
-    {itemsFromCart.length !== 0 ? <h2 className='text-center'>Items from cart</h2> : <h2 className='text-center'>Cart is empty</h2>}
+    <div className='cart-main-container'>
+    {itemsFromCart.length !== 0 ? <div className='cart-title'>
+              Items from cart
+              <svg className='cart-icon-svg-title' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
+              </div> 
+              : 
+              <div className='text-center'>Cart is empty</div>}
     {itemsFromCart.length !== 0 && <button className="btn btn-success mb-2" onClick={() => checkIfLoggedInBeforeSubmit()}>Make final order</button>}
     {itemsFromCart.length !== 0 && <table id="table" className='table table-hover tableElement'>
         <thead className='thead-name'>
@@ -254,6 +259,52 @@ const CartComponent = () => {
             )}
         </tbody>
     </table>} 
+
+    <div className='cart-container'>
+    {
+      itemsFromCart.map((itemFromCart) => {
+          return(
+            <div className='cart-item-container'>
+              <div className='item-image-container'>                  
+                <img className='item-meal-pic' src={"data:image/png;base64," + itemFromCart.mealImage} alt=''/>                   
+              </div> 
+              
+              <div className='item-info-container'>
+                <div className='item-name'>
+                  {itemFromCart.mealName}
+                </div>
+                <div className='item-description'>
+                  {itemFromCart.mealDescription}
+                </div>
+                <div className='item-type'>
+                  {itemFromCart.mealTypeName}
+                </div>
+                <div className='item-price'>
+                  {itemFromCart.mealPrice},00 RSD
+                </div>
+                <div className='item-quantity'>
+                  x{itemFromCart.quantity}
+                </div>
+              </div>
+
+              <div className='item-action-container'>
+                <button className='btn-action update' onClick={() =>handleShowEdit(itemFromCart)}>
+                  Update
+                  <svg className='action-icon' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>
+                </button>
+                <button className='btn-action delete' onClick={() => alertAreYouSureDelete(itemFromCart.mealId)}>
+                  Delete
+                  <svg className='action-icon' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
+                </button> 
+              </div>
+          </div>
+          )
+      })
+    }
+    </div>
+
+
+
 
 </div>
 
