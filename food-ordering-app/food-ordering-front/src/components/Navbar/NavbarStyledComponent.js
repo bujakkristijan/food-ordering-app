daee1df07ff3f3293b77a44318a6650ebf13ae7e
Kristijan Bujak <kbujak996@gmail.com>
2023-11-20 15:55:27 +0100

    navbar reorganizovan

--- food-ordering-app/food-ordering-front/src/components/Navbar/NavbarStyledComponent.js
+++ food-ordering-app/food-ordering-front/src/components/Navbar/NavbarStyledComponent.js
@@ -71,16 +71,15 @@ const NavbarStyledComponent = () => {
                {localStorage.role!="ADMIN" && localStorage.role!="EMPLOYEE" && <Link className='navLinkMenu' to='/menu' >
                     Menu
                </Link>}
-          <div className='nav-links-buttons-container'>
-               <div className='navMenu'>
-                    {localStorage.role!="ADMIN" && localStorage.role!="EMPLOYEE" && <Link className='cartLink' to='/cart' >
+               {localStorage.role!="ADMIN" && localStorage.role!="EMPLOYEE" && <Link className='cartLink' to='/cart' >
                          <div className='number-cart-div'>
                               <div className='number-cart'>{sumQuantityFromCartItems}</div>
                          </div>
                          <svg className='logo-cart' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
                          {/* <img className='logo-cart' src={require('../../images/cart2.png')} alt=''/>          */}
                     </Link>}
-
+          <div className='nav-links-buttons-container'>
+               {(localStorage.role ==="ADMIN" || localStorage.role ==="USER" || localStorage.role ==="EMPLOYEE") && <div className='navMenu'>
                     {localStorage.role==="ADMIN" && <svg class="x-icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>}
                     {localStorage.role==="ADMIN" && <Link className='navLink' id='employeeLink' to='/employees' >
                     Employees
@@ -102,7 +101,7 @@ const NavbarStyledComponent = () => {
                          Active orders
                     </Link>}
                     {localStorage.role==="USER" && <svg class="x-icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>}
-                    {localStorage.role==="USER" && <Link className='myProfileLink myProfile' to='/my-profile' >  
+                    {localStorage.role==="USER" && <Link className='myProfileLink' to='/my-profile' >  
                          My Profile      
                     </Link>}
                     {localStorage.role==="USER" && <Link className='myProfileLink' to='/my-active-final-orders' >
@@ -111,10 +110,10 @@ const NavbarStyledComponent = () => {
                     {localStorage.role==="USER" && <Link className='myProfileLink' to='/my-delivered-final-orders' >                  
                          My order history                   
                     </Link>}
-               </div>
+               </div>}
 
               
-          </div>
+          
 
           <div className='navBtn'>
                 <Link className='registrationLink' to='/registration' > 
@@ -131,6 +130,7 @@ const NavbarStyledComponent = () => {
                </button>}
           </div>
           <svg class="bars-icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+     </div>
      </div>
   )
 }
