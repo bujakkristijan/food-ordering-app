17e42252ec97e7497b49b1c5ff58afd09977a20c
Kristijan Bujak <kbujak996@gmail.com>
2023-11-22 13:56:12 +0100

    prikazuje se poruka ako je menu prazan za odredjeni meal-type (category). Nakon fetchovanja meal-a sa servera, setuje se state i radi se conditional rendering u jsx, jer mi je bio problem da se prikaze poruka na milisekund pa se prikazu podaci nakon ucitavanja. Sad je resen taj problem

--- food-ordering-app/food-ordering-front/src/components/meals-by-meal-type/ListMealByMealTypeComponent.js
+++ food-ordering-app/food-ordering-front/src/components/meals-by-meal-type/ListMealByMealTypeComponent.js
@@ -39,6 +39,8 @@ const ListMealByMealTypeComponent = () => {
 
     const navigate = useNavigate();
 
+    const [dataFetchingComplete, setDataFetchingComplete] = useState(false);
+
     useEffect(() => {
         getMealsByMealTypeId();
     }, [])
@@ -77,6 +79,7 @@ const ListMealByMealTypeComponent = () => {
     const getMealsByMealTypeId = () =>{
         MealService.getMealsByMealTypeId(mealTypeId).then((response)=>{
             setMeals(response.data);
+            setDataFetchingComplete(true);
             console.log("meals" + JSON.stringify(response.data));
         }).catch(error => {
             console.log(error);
@@ -120,15 +123,14 @@ const ListMealByMealTypeComponent = () => {
 
   return (
     <>
-    <div className='container-meal-by-meal-type'>
-            
-            
-            {meals.length !== 0 && <div className='meal-type-title'>
+    {dataFetchingComplete && meals.length === 0 && <div className='title-empty-meal-type'>Sorry, there are no offers for this category</div>}
+    {meals.length !== 0 && <div className='container-meal-by-meal-type'>
+            <div className='meal-type-title'>
                     <div className='meal-type-title-text'>
                         <div className='meal-type-title-desc'>Category: </div>
                         {meals[0].mealType.typeName}
                     </div>
-                </div>}
+                </div>
 
             <div className='container-meals-by-meal-type'>
                 {
@@ -157,10 +159,8 @@ const ListMealByMealTypeComponent = () => {
             <button className="btn-go-to-cart" onClick={navigateToCart}>
                 Go to cart
                 <svg className='cart-icon' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
-            </button>
-          
-
-        </div>
+            </button> 
+        </div>}
 
         <Modal show={show} onHide={handleClose} dialogClassName="modalCustomInsert" className="d-flex align-items-center justify-content-center">
         <Modal.Header closeButton>
