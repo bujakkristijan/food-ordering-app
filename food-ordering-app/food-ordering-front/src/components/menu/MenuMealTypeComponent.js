d6eaffc9595f65060202af448780b10556a046ed
Kristijan Bujak <kbujak996@gmail.com>
2023-11-23 16:32:55 +0100

    dodata ikonica kod show offers button-a

--- food-ordering-app/food-ordering-front/src/components/menu/MenuMealTypeComponent.js
+++ food-ordering-app/food-ordering-front/src/components/menu/MenuMealTypeComponent.js
@@ -31,7 +31,10 @@ const getAllMealTypes = () =>{
                         <div className='description-container'>
                             <p>{mealType.description}</p>
                         </div>
-                        <Link className='btn-see-items' to={`/meals-by-meal-type/${mealType.id}`}>See offers</Link>
+                        <Link className='btn-see-items' to={`/meals-by-meal-type/${mealType.id}`}>
+                            Show offers
+                            <svg className='show-items-icon' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M64 144a48 48 0 1 0 0-96 48 48 0 1 0 0 96zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM64 464a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm48-208a48 48 0 1 0 -96 0 48 48 0 1 0 96 0z"/></svg>
+                        </Link>
                     </div>
                 )
             })
